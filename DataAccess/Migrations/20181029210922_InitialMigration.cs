2648c42bb6c0c2fb881b4d43623ee8c2cb226fb9
Alex Boicu <boicualexandru@mail.com>
2018-10-29 23:10:27 +0200

    Added InitialMigration

---
+++ DataAccess/Migrations/20181029210922_InitialMigration.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dbo");
+
+            migrationBuilder.CreateTable(
+                name: "Menus",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Menus", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RestaurantMaps",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RestaurantMaps", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Email = table.Column<string>(maxLength: 50, nullable: false),
+                    HashPassword = table.Column<string>(nullable: false),
+                    FullName = table.Column<string>(maxLength: 100, nullable: true),
+                    Role = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MenuItems",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    MenuId = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(maxLength: 200, nullable: false),
+                    Price = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MenuItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MenuItems_Menus_MenuId",
+                        column: x => x.MenuId,
+                        principalSchema: "dbo",
+                        principalTable: "Menus",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RestaurantTables",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    RestaurantMapId = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(maxLength: 200, nullable: true),
+                    Seats = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RestaurantTables", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RestaurantTables_RestaurantMaps_RestaurantMapId",
+                        column: x => x.RestaurantMapId,
+                        principalSchema: "dbo",
+                        principalTable: "RestaurantMaps",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Restaurants",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 200, nullable: false),
+                    Description = table.Column<string>(maxLength: 500, nullable: true),
+                    Contact = table.Column<string>(maxLength: 200, nullable: true),
+                    ManagerId = table.Column<int>(nullable: false),
+                    MenuId = table.Column<int>(nullable: false),
+                    RestaurantMapId = table.Column<int>(nullable: false),
+                    City = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Restaurants", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Restaurants_Users_ManagerId",
+                        column: x => x.ManagerId,
+                        principalSchema: "dbo",
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Restaurants_Menus_MenuId",
+                        column: x => x.MenuId,
+                        principalSchema: "dbo",
+                        principalTable: "Menus",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Restaurants_RestaurantMaps_RestaurantMapId",
+                        column: x => x.RestaurantMapId,
+                        principalSchema: "dbo",
+                        principalTable: "RestaurantMaps",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reservations",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<int>(nullable: false),
+                    RestaurantId = table.Column<int>(nullable: false),
+                    ParticipantsCount = table.Column<int>(nullable: false),
+                    StartTime = table.Column<DateTime>(nullable: false),
+                    EndTime = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reservations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reservations_Restaurants_RestaurantId",
+                        column: x => x.RestaurantId,
+                        principalSchema: "dbo",
+                        principalTable: "Restaurants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RestaurantEmployees",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    RestaurantId = table.Column<int>(nullable: false),
+                    UserId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RestaurantEmployees", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RestaurantEmployees_Restaurants_RestaurantId",
+                        column: x => x.RestaurantId,
+                        principalSchema: "dbo",
+                        principalTable: "Restaurants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RestaurantEmployees_Users_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "dbo",
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ReservedTables",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ReservationId = table.Column<int>(nullable: false),
+                    RestaurantTableId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReservedTables", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ReservedTables_Reservations_ReservationId",
+                        column: x => x.ReservationId,
+                        principalSchema: "dbo",
+                        principalTable: "Reservations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ReservedTables_RestaurantTables_RestaurantTableId",
+                        column: x => x.RestaurantTableId,
+                        principalSchema: "dbo",
+                        principalTable: "RestaurantTables",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MenuItems_MenuId",
+                schema: "dbo",
+                table: "MenuItems",
+                column: "MenuId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_RestaurantId",
+                schema: "dbo",
+                table: "Reservations",
+                column: "RestaurantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReservedTables_ReservationId",
+                schema: "dbo",
+                table: "ReservedTables",
+                column: "ReservationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReservedTables_RestaurantTableId",
+                schema: "dbo",
+                table: "ReservedTables",
+                column: "RestaurantTableId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RestaurantEmployees_RestaurantId",
+                schema: "dbo",
+                table: "RestaurantEmployees",
+                column: "RestaurantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RestaurantEmployees_UserId",
+                schema: "dbo",
+                table: "RestaurantEmployees",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Restaurants_ManagerId",
+                schema: "dbo",
+                table: "Restaurants",
+                column: "ManagerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Restaurants_MenuId",
+                schema: "dbo",
+                table: "Restaurants",
+                column: "MenuId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Restaurants_RestaurantMapId",
+                schema: "dbo",
+                table: "Restaurants",
+                column: "RestaurantMapId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RestaurantTables_RestaurantMapId",
+                schema: "dbo",
+                table: "RestaurantTables",
+                column: "RestaurantMapId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MenuItems",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "ReservedTables",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "RestaurantEmployees",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "Reservations",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "RestaurantTables",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "Restaurants",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "Users",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "Menus",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "RestaurantMaps",
+                schema: "dbo");
+        }
+    }
+}
