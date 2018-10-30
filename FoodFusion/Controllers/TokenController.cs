87e266be93b9c554698dae6acb14685d7e7b3258
Alex Boicu <a.boicu@levi9.com>
2018-10-30 16:42:42 +0200

    Added TokenController

---
+++ FoodFusion/Controllers/TokenController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Services.Authentication;
+using IAuthenticationService = Services.Authentication.IAuthenticationService;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly IAuthenticationService _authenticationService;
+
+        public TokenController(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+
+        // POST: api/Token
+        [HttpPost]
+        public IActionResult Post([FromBody] LoginModel loginModel)
+        {
+            try
+            {
+                var token = _authenticationService.GetToken(loginModel);
+                return Ok(token);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Invalid Credentials.");
+            }
+        }
+    }
+}
