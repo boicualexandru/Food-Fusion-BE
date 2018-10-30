e330963beaa54b27a91d5e43b650018fa7efa3ad
Alex Boicu <a.boicu@levi9.com>
2018-10-30 08:48:37 +0200

    Configured Jwt on Startup class

--- FoodFusion/Startup.cs
+++ FoodFusion/Startup.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
 using DataAccess.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -14,6 +16,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using Swashbuckle.AspNetCore.Swagger;
 
 namespace FoodFusion
@@ -46,6 +49,21 @@ namespace FoodFusion
                     options.UseSqlServer(Configuration.GetSection("ConnectionStrings:FoodFusionDB").Value)
                         .ConfigureWarnings(warning => warning.Ignore(CoreEventId.IncludeIgnoredWarning)),
                     optionsLifetime: ServiceLifetime.Scoped);
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                var jwtKeyString = Configuration.GetSection("Jwt:SymetricSecurityKey").Value;
+                var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKeyString));
+                options.TokenValidationParameters.IssuerSigningKey = symmetricSecurityKey;
+                
+                options.TokenValidationParameters.ValidateLifetime = true;
+                options.TokenValidationParameters.ClockSkew = TimeSpan.Zero;
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
