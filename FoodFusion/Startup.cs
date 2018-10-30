2a7e520dd6608c4bcfd9c45fd02b241d5e0ff4e3
Alex Boicu <a.boicu@levi9.com>
2018-10-30 18:00:02 +0200

    Fixed Swagger to use xml comments

--- FoodFusion/Startup.cs
+++ FoodFusion/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
@@ -41,7 +43,14 @@ namespace FoodFusion
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new Info { Title = "Food Fusion API", Version = "v1" });
+                
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath, true);
             });
+            
+            services.Configure<JwtSettings>(Configuration.GetSection("Jwt"));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
