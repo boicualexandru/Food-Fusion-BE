38e55fb2485d28dcd0a61529a19f8077ef754b99
Alex Boicu <boicualexandru@mail.com>
2018-11-04 00:13:23 +0200

    MenuService (work on progress)

---
+++ Authentication/Menus/IMenuService.cs
@@ -0,0 +1,12 @@
+﻿using Services.Menus.Models;
+
+namespace Services.Menus
+{
+    interface IMenuService
+    {
+        MenuModel ReplaceMenu(int restaurantId, MenuModel menuModel);
+        MenuModel GetMenu(int restaurantId);
+        MenuItemModel AddItem(int restaurantId, MenuItemModel menuItemModel);
+        void RemoveItem(int menuItemId);
+    }
+}
---
+++ Authentication/Menus/MenuService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using DataAccess.Models;
+using Services.Menus.Models;
+
+namespace Services.Menus
+{
+    class MenuService : IMenuService
+    {
+        private readonly FoodFusionContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public MenuService(FoodFusionContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public MenuModel ReplaceMenu(int restaurantId, MenuModel menuModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MenuModel GetMenu(int restaurantId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MenuItemModel AddItem(int restaurantId, MenuItemModel menuItemModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveItem(int menuItemId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
