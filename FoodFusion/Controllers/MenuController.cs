f18f59a5c96b8e8d788564c62d9dfb31c2d66a9b
Alex Boicu <boicualexandru@mail.com>
2018-11-11 15:45:48 +0200

    Resource based authorization for MenuController

--- FoodFusion/Controllers/MenuController.cs
+++ FoodFusion/Controllers/MenuController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Services.Authorization;
 using Services.Menus;
 using Services.Menus.Exceptions;
 using Services.Menus.Models;
-using Services.Restaurants.Exceptions;
+using Services.Restaurants;
 
 namespace WebApi.Controllers
 {
@@ -10,10 +12,20 @@ namespace WebApi.Controllers
     [ApiController]
     public class MenuController : ControllerBase
     {
+        private readonly IResourceAuthorizationService<RestaurantAuthorizationRequirement> _restaurantAuthorizationService;
+        private readonly IResourceAuthorizationService<MenuAuthorizationRequirement> _menuAuthorizationService;
+        private readonly IResourceAuthorizationService<MenuItemAuthorizationRequirement> _menuItemAuthorizationService;
         private readonly IMenuService _menuService;
 
-        public MenuController(IMenuService menuService)
+        public MenuController(
+            IResourceAuthorizationService<RestaurantAuthorizationRequirement> restaurantAuthorizationService, 
+            IResourceAuthorizationService<MenuAuthorizationRequirement> menuAuthorizationService, 
+            IResourceAuthorizationService<MenuItemAuthorizationRequirement> menuItemAuthorizationService, 
+            IMenuService menuService)
         {
+            _restaurantAuthorizationService = restaurantAuthorizationService;
+            _menuAuthorizationService = menuAuthorizationService;
+            _menuItemAuthorizationService = menuItemAuthorizationService;
             _menuService = menuService;
         }
 
@@ -26,17 +38,14 @@ namespace WebApi.Controllers
                 var menu = _menuService.GetMenu(restaurantId);
                 return Ok(menu);
             }
-            catch (RestaurantNotFoundException)
-            {
-                return BadRequest();
-            }
             catch (MenuNotFoundException)
             {
-                return BadRequest();
+                return NotFound();
             }
         }
 
         // POST: api/Restaurants/5/Menu
+        [Authorize]
         [HttpPost("{restaurantId}/[controller]")]
         public IActionResult Post(int restaurantId, [FromBody] MenuModel menu)
         {
@@ -45,41 +54,42 @@ namespace WebApi.Controllers
                 return BadRequest(ModelState);
             }
 
-            try
-            {
-                var createdMenu = _menuService.AddMenuIfNotExists(restaurantId, menu);
-                return Ok(createdMenu);
-            }
-            catch (RestaurantNotFoundException)
-            {
-                return BadRequest();
-            }
-            catch (MenuAlreadyExistsException)
-            {
-                return BadRequest();
-            }
+            var isAuthorized = _restaurantAuthorizationService
+                .WithUser(User)
+                .WithRequirement(Operations<RestaurantAuthorizationRequirement>.Update)
+                .WithResource(restaurantId)
+                .IsAuthorized();
+            if (!isAuthorized) return Forbid();
+
+            var createdMenu = _menuService.AddMenuIfNotExists(restaurantId, menu);
+            return Ok(createdMenu);
         }
 
         // DELETE: api/Restaurants/5/Menu
+        [Authorize]
         [HttpDelete("{restaurantId}/[controller]")]
         public IActionResult Delete(int restaurantId)
         {
             try
             {
+                var isAuthorized = _restaurantAuthorizationService
+                   .WithUser(User)
+                   .WithRequirement(Operations<RestaurantAuthorizationRequirement>.Update)
+                   .WithResource(restaurantId)
+                   .IsAuthorized();
+                if (!isAuthorized) return Forbid();
+
                 _menuService.RemoveMenu(restaurantId);
                 return Ok();
             }
-            catch (RestaurantNotFoundException)
-            {
-                return BadRequest();
-            }
             catch (MenuNotFoundException)
             {
-                return BadRequest();
+                return NotFound();
             }
         }
 
         // POST: api/Restaurants/Menu/5/Items
+        [Authorize]
         [HttpPost("[controller]/{menuId}/Items")]
         public IActionResult PostItem(int menuId, [FromBody] MenuItemModel item)
         {
@@ -90,16 +100,24 @@ namespace WebApi.Controllers
 
             try
             {
+                var isAuthorized = _menuAuthorizationService
+                   .WithUser(User)
+                   .WithRequirement(Operations<MenuAuthorizationRequirement>.Update)
+                   .WithResource(menuId)
+                   .IsAuthorized();
+                if (!isAuthorized) return Forbid();
+
                 var createdItem = _menuService.AddItem(menuId, item);
                 return Ok(createdItem);
             }
             catch (MenuNotFoundException)
             {
-                return BadRequest();
+                return NotFound();
             }
         }
 
         // PUT: api/Restaurants/Menu/Items/5
+        [Authorize]
         [HttpPut("[controller]/Items/{itemId}")]
         public IActionResult UpdateItem(int itemId, [FromBody] MenuItemModel item)
         {
@@ -110,28 +128,43 @@ namespace WebApi.Controllers
 
             try
             {
+                var isAuthorized = _menuItemAuthorizationService
+                      .WithUser(User)
+                      .WithRequirement(Operations<MenuItemAuthorizationRequirement>.Update)
+                      .WithResource(itemId)
+                      .IsAuthorized();
+                if (!isAuthorized) return Forbid();
+
                 item.Id = itemId;
                 _menuService.UpdateItem(item);
                 return Ok(item);
             }
             catch (MenuItemNotFoundException)
             {
-                return BadRequest();
+                return NotFound();
             }
         }
 
         // DELETE: api/Restaurants/Menu/Items/5
+        [Authorize]
         [HttpDelete("[controller]/Items/{itemId}")]
         public IActionResult DeleteItem(int itemId)
         {
             try
             {
+                var isAuthorized = _menuItemAuthorizationService
+                      .WithUser(User)
+                      .WithRequirement(Operations<MenuItemAuthorizationRequirement>.Delete)
+                      .WithResource(itemId)
+                      .IsAuthorized();
+                if (!isAuthorized) return Forbid();
+
                 _menuService.RemoveItem(itemId);
                 return Ok();
             }
             catch (MenuItemNotFoundException)
             {
-                return BadRequest();
+                return NotFound();
             }
         }
     }
---
+++ Services/Menus/MenuItemAuthorizationHandler.cs
@@ -0,0 +1,74 @@
+﻿using DataAccess.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using System.Linq;
+using System.Threading.Tasks;
+using Services.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Services.Restaurants.Exceptions;
+using System.Security.Claims;
+using Services.Menus.Exceptions;
+using Services.Authorization.Exceptions;
+
+namespace Services.Menus
+{
+    /// <summary>
+    /// Authorizations:
+    /// Read                    - All Users
+    /// Create, Update, Delete  - Manager
+    /// </summary>
+    public class MenuItemAuthorizationHandler :
+    AuthorizationHandler<MenuItemAuthorizationRequirement, int>
+    {
+        private readonly FoodFusionContext _dbContext;
+
+        public MenuItemAuthorizationHandler(FoodFusionContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context,
+                                                       MenuItemAuthorizationRequirement requirement,
+                                                       int menuItemId = 0)
+        {
+            // Grant read for all
+            if(requirement.Name == Operations<MenuItemAuthorizationRequirement>.Read.Name)
+            {
+                context.Succeed(requirement);
+                return Task.CompletedTask;
+            }
+
+            // Grant all rights for Admin
+            var isAdmin = context.User.IsInRole(UserRole.Admin.ToString());
+            if (isAdmin)
+            {
+                context.Succeed(requirement);
+                return Task.CompletedTask;
+            }
+
+            var userIdClaim = context.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (!int.TryParse(userIdClaim, out var userId)) throw new InvalidClaimException();
+
+            var menuItem = _dbContext.MenuItems
+                .AsNoTracking()
+                .Include(i => i.Menu)
+                    .ThenInclude(m => m.Restaurant)
+                        .ThenInclude(r => r.Manager)
+                .FirstOrDefault(i => i.Id == menuItemId);
+            menuItem = menuItem ?? throw new MenuItemNotFoundException();
+            menuItem.Menu.Restaurant.Manager = menuItem.Menu.Restaurant.Manager ?? throw new ManagerNotFoundException();
+            
+            if (userId != menuItem.Menu.Restaurant.Manager.Id)
+            {
+                context.Fail();
+                return Task.CompletedTask;
+            }
+
+            // Grant All Rights for Restaurant Manager
+            context.Succeed(requirement);
+            return Task.CompletedTask;
+        }
+    }
+
+    public class MenuItemAuthorizationRequirement : OperationAuthorizationRequirement { }
+}
