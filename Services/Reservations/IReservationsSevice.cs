72db17d88f84b7f78d7cf6d46ebb212c16ec1b58
Alex Boicu <boicualexandru@mail.com>
2018-11-14 22:49:08 +0200

    Reservation Service Interface (work on progress)

---
+++ Services/Reservations/IReservationsSevice.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Services.Reservations
+{
+    public interface IReservationsService
+    {
+        // TODO: filter by date and by upcoming/old ones
+        IList<ReservationModel> GetUserReservations(int userId);
+        IList<ReservationModel> GetRestaurantReservations(int restaurantId);
+        ReservationDetailedModel GetReservation(int reservationId);
+        IList<TimeRange> GetAvailability(int restaurantId, int participantsCount, TimeRange timeRange);
+        ReservationDetailedModel AddReservation(ReservationDetailedModel reservation);
+        ReservationDetailedModel UpdateReservation(ReservationDetailedModel reservation);
+        void RemoveReservation(int reservationId);
+    }
+
+    public class TimeRange
+    {
+    }
+
+    public class ReservationDetailedModel
+    {
+    }
+
+    public class ReservationModel
+    {
+    }
+}
