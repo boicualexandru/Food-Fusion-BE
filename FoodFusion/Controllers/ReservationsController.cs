97c1864790cef58424dd4a0113c12a7ca1837c8e
Alex Boicu <boicualexandru@mail.com>
2018-11-18 20:21:15 +0200

    ReservationsController (work on progress)

---
+++ FoodFusion/Controllers/ReservationsController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Common;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services.Reservations;
+
+namespace WebApi.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class ReservationsController : ControllerBase
+    {
+        private readonly IReservationsService _rReservationsService;
+
+        public ReservationsController(IReservationsService rReservationsService)
+        {
+            _rReservationsService = rReservationsService;
+        }
+
+        // GET: api/Reservations
+        [HttpGet("Restaurants/{restaurantId}/availability")]
+        public IActionResult Get(
+            [FromRoute] int restaurantId, 
+            [FromQuery] int participantsCount, 
+            [FromQuery] DateTime start,
+            [FromQuery] DateTime end)
+        {
+            var range = new TimeRange { Start = start, End = end };
+            var availability = _rReservationsService.GetAvailability(
+                restaurantId, participantsCount, range);
+
+            return Ok(availability);
+        }
+
+        // GET: api/Reservations/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Reservations
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Reservations/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
