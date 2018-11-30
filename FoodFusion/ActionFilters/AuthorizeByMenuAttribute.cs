73b5007a887ab733baf7efaf431f33c88d5dc16c
Alex Boicu <boicualexandru@mail.com>
2018-11-30 20:51:55 +0200

    Added resource-oriented authorize attributes for Restaurants and Menus

---
+++ FoodFusion/ActionFilters/AuthorizeByMenuAttribute.cs
@@ -0,0 +1,36 @@
+﻿using DataAccess.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace WebApi.ActionFilters
+{
+    public class AuthorizeByMenuAttribute : TypeFilterAttribute
+    {
+        public AuthorizeByMenuAttribute(string roles, string key = "menuId") : base(typeof(AuthorizeByMenuFilter))
+        {
+            Arguments = new object[] { roles, key };
+        }
+
+        private class AuthorizeByMenuFilter : AuthorizeByRestaurantAttribute
+        {
+            private readonly FoodFusionContext _dbContext;
+
+            public AuthorizeByMenuFilter(FoodFusionContext dbContext, string roles, string key) : base(roles, key)
+            {
+                _dbContext = dbContext;
+            }
+            
+            override protected string GetRestaurantId(string keyValue)
+            {
+                var restaurantId = _dbContext.Menus
+                    .AsNoTracking()
+                    .Where(m => m.Id == int.Parse(keyValue))
+                    .Select(m => m.RestaurantId)
+                    .FirstOrDefault();
+
+                return restaurantId.ToString();
+            }
+        }
+    }
+}
---
+++ FoodFusion/ActionFilters/AuthorizeByRestaurantAttribute.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Services.Authentication.Models;
+using System;
+using System.Linq;
+using System.Security.Claims;
+
+namespace WebApi.ActionFilters
+{
+    public class AuthorizeByRestaurantAttribute : Attribute, IResourceFilter
+    {
+        public string Roles
+        {
+            get { return string.Join(',', _roles); }
+            set { _roles = value.Split(',').Select(r => r.Trim()).ToArray(); }
+        }
+        public string Key { get; set; } = "restaurantId";
+
+        private string[] _roles;
+
+        public AuthorizeByRestaurantAttribute() { }
+
+        public AuthorizeByRestaurantAttribute(string roles, string key = null)
+        {
+            Roles = roles;
+            Key = key ?? Key;
+        }
+
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+
+            var requireAdmin = _roles.Contains("Admin");
+            if (requireAdmin)
+            {
+                var hasAdminRole = context.HttpContext.User.Claims.Any(c => c.Type == ClaimTypes.Role && c.Value == "Admin");
+                if (hasAdminRole) return;
+            }
+            
+            var keyValue = context.RouteData.Values[Key].ToString();
+            var restaurantId = GetRestaurantId(keyValue);
+
+            var requireManager = _roles.Contains("Manager");
+            if (requireManager)
+            {
+                var isManagerForThisRestaurant = context.HttpContext
+                        .User.Claims.Any(c => c.Type == CustomDefinedClaimNames.ManagedRestaurant && c.Value == restaurantId);
+                if (isManagerForThisRestaurant) return;
+            }
+
+            var requireEmployee = _roles.Contains("Employee");
+            if (requireEmployee)
+            {
+                var isEmployeeForThisRestaurant = context.HttpContext
+                        .User.Claims.Any(c => c.Type == CustomDefinedClaimNames.EmployeeOfRestaurant && c.Value == restaurantId);
+                if (isEmployeeForThisRestaurant) return;
+            }
+            
+            context.Result = new ForbidResult();
+        }
+
+        public void OnResourceExecuted(ResourceExecutedContext context) { }
+
+        protected virtual string GetRestaurantId(string keyValue) => keyValue;
+    }
+}
