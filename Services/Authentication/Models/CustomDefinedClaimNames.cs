4cc783eb45c057b66527412f4a100359468c339f
Alex Boicu <boicualexandru@mail.com>
2018-11-30 14:34:19 +0200

    Added Claims for manager and emploee of restaurants

--- Services/Authentication/Models/CustomDefinedClaimNames.cs
+++ Services/Authentication/Models/CustomDefinedClaimNames.cs
@@ -3,5 +3,7 @@
     public static class CustomDefinedClaimNames
     {
         public const string FullName = "full_name";
+        public const string ManagedRestaurant = "managed_restaurant";
+        public const string EmployeeOfRestaurant = "employee_of_restaurant";
     }
 }
