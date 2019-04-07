6bc589dd610f9305b1985a91aa0e8a7fff11b913
Alex Boicu <boicualexandru@mail.com>
2019-04-07 15:25:14 +0300

    Hotel DbSets

---
+++ DataAccess/Models/HotelRoom.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataAccess.Models
+{
+    public class HotelRoom
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Name { get; set; }
+
+        [Required]
+        public int Floor { get; set; }
+
+        [Required]
+        public int Beds { get; set; }
+
+        [Required]
+        public int MaxGuests { get; set; }
+
+
+        public virtual ICollection<HotelRoomFeature> RoomFeatures { get; set; }
+    }
+}
---
+++ DataAccess/Models/HotelRoomReservation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataAccess.Models
+{
+    public class HotelRoomReservation
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public int RoomId { get; set; }
+
+        public int UserId { get; set; }
+
+        public int GuestsCount { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+    }
+}
